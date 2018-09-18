c52aa76b98fe507f67f1d710719d23a52b996553
ganhao <ganhaogood@126.com>
2018-09-18 20:28:32 +0800

    initt

---
+++ src/redux/reducers.js
@@ -0,0 +1,23 @@
+import {combineReducers} from "redux"
+
+
+const initXxx=1
+function xxx(state=initXxx,action) {
+  switch (action.type){
+    default:
+      return state
+  }
+}
+
+const initYyy={}
+function yyy(state=initYyy,action) {
+  switch (action.type){
+    default:
+      return state
+  }
+}
+
+export default combineReducers({
+  xxx,
+  yyy
+})
